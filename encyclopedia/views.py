cbb66c3e9b9b83322b0b9d3b818658f0aaa1dfa3
Minh Vy Ha <vyha0512@gmail.com>
2022-07-19 19:51:28 +1000

    update

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,10 @@
+from re import L
 from django.shortcuts import render
-
+from django import forms
 from . import util
+
+class AddForm(forms.Form):
+    markdown = forms.CharField(widget=forms.Textarea)
 
 
 def index(request):
@@ -10,4 +14,10 @@ def index(request):
 
 
 def add(request):
-    return render(request, "encyclopedia/add.html")
+    if request.method == 'POST':
+        form = AddForm(request.POST)
+        if form.is_valid():
+            ...
+    return render(request, "encyclopedia/add.html", {
+        "form" : AddForm()
+    })
