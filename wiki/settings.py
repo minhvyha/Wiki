9ab02009fcb4627f2eadb0e9df2e2e71b640d32c
Minh Vy Ha <vyha0512@gmail.com>
2022-07-24 13:03:28 +1000

    update

--- wiki/settings.py
+++ wiki/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
